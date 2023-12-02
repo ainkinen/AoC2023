42586ed9f525bcb838a8859efb3fb7fa0dd76e41
Antti-Jussi Inkinen <aj.inkinen@gmail.com>
2023-12-02 17:41:20 +0200

    Add a day template directory

---
+++ aoc2023/dayX/__init__.py
@@ -0,0 +1,12 @@
+import os
+from typing import Any
+
+INPUT_PATH = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def part1(input_path: str = INPUT_PATH) -> Any:
+    return
+
+
+def part2(input_path: str = INPUT_PATH) -> Any:
+    return
