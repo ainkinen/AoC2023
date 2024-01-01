8273074ac8402096768563097500b5e6c4dfbb86
Antti-Jussi Inkinen <aj.inkinen@gmail.com>
2024-01-02 01:52:45 +0200

    Day 22

---
+++ aoc2023/day22/day22_test.py
@@ -0,0 +1,14 @@
+import os
+
+from aoc2023.day22 import part1, part2
+
+TEST_INPUT_PATH = os.path.join(os.path.dirname(__file__), "test_input.txt")
+TEST_INPUT_2_PATH = os.path.join(os.path.dirname(__file__), "test_input_2.txt")
+
+
+def test_part1():
+    assert part1(TEST_INPUT_PATH) == 5
+
+
+def test_part2():
+    assert part2(TEST_INPUT_2_PATH) == 7
